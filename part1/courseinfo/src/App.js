6e5d7738d8114db6825db491b6d8feb43102df5a
ShaCP <47069245+ShaCP@users.noreply.github.com>
2021-07-26 20:01:06 -0400

    1.5: course information, step5 - completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ courseName }) => <h1>{courseName}</h1>;
 
 const Part = ({ part, exercises }) => (
   <p>
@@ -23,18 +23,20 @@ const Total = ({ parts: [part1, part2, part3] }) => (
 );
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
