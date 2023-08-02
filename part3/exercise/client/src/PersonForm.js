3f760a4ff53bd98727979300ba8d3378d0561c3f
Shafick Cure <scp8698@gmail.com>
2023-08-02 15:25:42 -0400

    created workspace with client and server

---
+++ part3/exercise/client/src/PersonForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Input from './Input';
+
+const PersonForm = ({
+  addPerson,
+  newName,
+  newNumber,
+  setNewName,
+  setNewNumber,
+}) => (
+  <div class="form-container">
+    <form class="person-form" onSubmit={addPerson}>
+      <Input
+        label="name"
+        value={newName}
+        onChange={(e) => setNewName(e.target.value)}
+      />
+      <Input
+        label="phone number"
+        value={newNumber}
+        onChange={(e) => setNewNumber(e.target.value)}
+      />
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  </div>
+);
+
+export default PersonForm;
